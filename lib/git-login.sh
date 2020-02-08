e72985f22a490a1380cbe7c00079c209e02ceb07
matsume-hiro <matsume@techflyer.co.jp>
2020-02-09 01:43:21 +0900

    git branch

--- lib/git-login.sh
+++ lib/git-login.sh
@@ -4,7 +4,7 @@ git config --global user.email "${GITHUB_USEREMAIL}"
 
 git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
-git checkout -b ${GITHUB_FROM_BRANCH}
+git checkout -b ${GITHUB_BRANCH}
 git branch -a
 
 ls -la
--- lib/git-push.sh
+++ lib/git-push.sh
@@ -2,4 +2,4 @@ echo "git push."
 git status
 git add ./docs
 git commit -m '[updater] update pdf.'
-git push -f origin ${GITHUB_RELEASE_BRANCH}
+git push -f origin HEAD
